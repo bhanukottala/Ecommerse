d7d4d33253d87f9a85aa61190c3abacb9c570700
bhanukottala <bhanukottala@gmail.com>
2021-03-02 19:31:44 +0530

    frst commit

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Banner=()=>{
+    return(
+        <header>
+            <div className="headerText">
+                <div>
+                 <h1>Bhanuchander</h1>
+                 <p>Enjoy Online Shopping</p>
+                 </div>
+            </div>
+
+        </header>
+        
+    )
+}
+export default Banner;
